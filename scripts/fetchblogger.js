010806be982777d3dc8fd1fa9bed3855f464ec8e
romanr00776-coder <romanr00776@gmail.com>
2025-09-16 19:39:50 +0530

    Update fetchblogger.js

--- scripts/fetchblogger.js
+++ scripts/fetchblogger.js
@@ -1,22 +1,9 @@
 const https = require('https');
 const fs = require('fs');
-
 const url = 'https://www.giggiplay.online/feeds/posts/default';
 
 https.get(url, (res) => {
-    let data = '';
-
-    res.on('data', (chunk) => {
-        data += chunk;
-    });
-
-    res.on('end', () => {
-        fs.writeFileSync('output.xml', data, 'utf8');
-        console.log('RSS feed downloaded successfully!');
-    });
-
-}).on('error', (err) => {
-    console.error('Error fetching RSS feed:', err.message);
-    fs.writeFileSync('fetchBlogger.error.log', err.stack || err.message, 'utf8');
-    process.exit(1);
+  let data = '';
+  res.on('data', (chunk) => { data += chunk; });
+  res.on('end', () => { fs.writeFileSync('output.xml', data); });
 });
